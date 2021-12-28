1bf09ad83fc5c22423179097ba390f8224504769
juhyojeong <juhyojeong58@gmail.com>
2021-12-28 21:32:04 +0900

    feat: Add firebase

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,6 +1,8 @@
+import "firebase/firestore";
 import { useState } from "react";
 import { FloatingLabel, Form, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { firestore } from "../firebase";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -16,12 +18,26 @@ const ErrorMsg = styled.p`
   font-size: 15px;
   color: red;
 `;
+const CheckBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+const StyledCheck = styled.p`
+  font-size: 15px;
+  margin-top: 10px;
+  margin-bottom: 0;
+  cursor: pointer;
+  :hover {
+    color: gray;
+  }
+`;
 
 function Signup() {
   const navigate = useNavigate();
   const [user, setUser] = useState({ email: "", username: "", password: "" });
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
+  const [checked, setChecked] = useState(false);
 
   const _handleChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
@@ -36,13 +52,24 @@ function Signup() {
       setErrorMsg("비밀번호를 6자 이상 입력해주세요");
     } else if (user.password !== passwordConfirm) {
       setErrorMsg("비밀번호가 일치하지 않습니다");
+    } else if (!checked) {
+      setErrorMsg("이름 중복확인을 해주세요");
     } else {
       setErrorMsg("");
-      navigate("/login");
+      firestore
+        .collection("user")
+        .add(user)
+        .then((res) => {
+          navigate("/login");
+        });
     }
   };
   const _handlePwdConfirm = (e) => {
     setPasswordConfirm(e.target.value);
+  };
+  const _handleCheck = () => {
+    setErrorMsg("이미 존재하는 이름입니다");
+    setChecked(false);
   };
 
   return (
@@ -54,7 +81,6 @@ function Signup() {
             value={user.email}
             onChange={_handleChange}
             type="email"
-            placeholder="Enter email"
           />
         </FloatingLabel>
         <FloatingLabel
@@ -68,6 +94,9 @@ function Signup() {
             onChange={_handleChange}
             type="text"
           />
+          <CheckBox>
+            <StyledCheck onClick={_handleCheck}>중복확인</StyledCheck>
+          </CheckBox>
         </FloatingLabel>
         <FloatingLabel
           className="mb-3"
